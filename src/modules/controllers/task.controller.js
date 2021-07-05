a986e016a04e02f5285db1781ff373e96d46920d
Andrey <asavostianiuk.exceedteam@gmail.com>
2021-07-05 12:22:32 +0300

    fixed line and tabs

--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -1,46 +1,45 @@
 const Task = require('../../db/models/task/index');
 
-
 module.exports.allTasks = (req, res, next) => {
-    Task.find().then(result => {
-            res.send({data: result})
-       });
+Task.find().then(result => {
+    res.send({data: result})
+});
 };
 
 module.exports.createTasks = (req, res, next) => {
-    const task = new Task(req.body);
+const task = new Task(req.body);
 
-    task.save().then(result1 => {
-        Task.find().then(result => {
-            res.send({data: result})
-        });
+task.save().then(result1 => {
+    Task.find().then(result => {
+        res.send({data: result})
     });
+});
 };
 
 module.exports.editTasks = (req, res, next) => {
-    Task.updateOne({
+Task.updateOne({
         text:req.body.text
-    }).then(result => {
-        Task.find().then(result => {
-            res.send({data: result})
-        });
-   });
+}).then(result => {
+    Task.find().then(result => {
+        res.send({data: result})
+    });
+});
 };
 
 module.exports.deleteTasks = (req, res, next) => {
-    Task.deleteOne({
-         _id: req.query._id
-    }).then(result => {
-        Task.find().then(result => {
-            res.send({data: result})
-        });
-   });
+Task.deleteOne({
+    _id: req.query._id
+}).then(result => {
+    Task.find().then(result => {
+        res.send({data: result})
+    });
+});
 };  
 
 module.exports.deleteAllTasks = (req, res, next) => {
-    Task.deleteMany().then(result => {
-        Task.find().then(result => {
-            res.send({data: result})
-        });
-   });
+Task.deleteMany().then(result => {
+    Task.find().then(result => {
+        res.send({data: result})
+    });
+ });
 };
